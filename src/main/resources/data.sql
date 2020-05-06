eefb25b8ffcd6e61efbc995dddf527e5fb171037
sawada <flumpool0421@yahoo.co.jp>
2020-05-06 18:45:24 +0900

    SpringTest MyBatis

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 /* 従業員テーブルのデータ*/
-INSERT INTO employee (employee_id, employee_name, age)
-VALUES(1, '山田太郎', 30);
+/*INSERT INTO employee (employee_id, employee_name, age)
+VALUES(1, '山田太郎', 30);*/
 
 /* ユーザーマスタのデータ（ADMIN権限） パスワードは「password」*/
 INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
